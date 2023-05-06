cf7dabced89ec544d37e17c77ffd38dd57ab7634
JASaddington <jasaddington@gmail.com>
2023-05-06 13:27:48 -0500

    set up npm packages in server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql= requrie('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({extended: false}));
+app,use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'roothere',
+    database: 'personnel_db'
+  },
+  console.log('connected to personnel_db database!')
+);
