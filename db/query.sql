0b256b7ccb60ed483862d175d7c4308c6a1b70e7
JASaddington <jasaddington@gmail.com>
2023-05-06 18:00:48 -0500

    added more to queries.sql

--- db/query.sql
+++ db/query.sql
@@ -1,24 +1,45 @@
-USE personnel_db;
+
 -- option VIEW ALL DEPARTMENTS
 USE personnel_db;
 SELECT * FROM department;
 
+-- =======================================
+
 -- option VIEW ALL EMPLOYEES
 USE personnel_db;
-
 SELECT 
+-- specifying column names and values
 employee.id AS id, 
 employee.first_name AS first_name, 
 employee.last_name AS last_name, 
 role.title AS title, 
 department.name AS department,
 role.salary AS salary,
+
+-- get the whole manager name
 CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
 
 FROM employee
 
+-- get role name
 JOIN role ON employee.role_id = role.id
+
+-- get department name
 JOIN department on role.department_id = department.id
+
+-- using alias 'manager' as the second instance of 'employee' table, to reference itself so that we can get the possible manager name for that employee.
 JOIN employee manager ON manager.id = employee.manager_id;
 
+-- =================================
 
+-- option VIEW ALL ROLES
+
+USE personnel_db;
+
+SELECT
+  role.id AS id,
+  role.title AS title,
+  department.name AS department,
+  role.salary AS salary
+  FROM role
+  JOIN department ON role.department_id = department.id;
