ebfa2b8705b607473071d28c38100ab67454ca87
JASaddington <jasaddington@gmail.com>
2023-05-06 17:23:35 -0500

    writing out queries for testing

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,24 @@
 USE personnel_db;
+-- option VIEW ALL DEPARTMENTS
+USE personnel_db;
+SELECT * FROM department;
 
-SELECT employee.id AS id,
+-- option VIEW ALL EMPLOYEES
+USE personnel_db;
+
+SELECT 
+employee.id AS id, 
+employee.first_name AS first_name, 
+employee.last_name AS last_name, 
+role.title AS title, 
+department.name AS department,
+role.salary AS salary,
+CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+
+FROM employee
+
+JOIN role ON employee.role_id = role.id
+JOIN department on role.department_id = department.id
+JOIN employee manager ON manager.id = employee.manager_id;
+
+
