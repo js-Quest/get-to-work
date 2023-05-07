dba2f2a276918971ae8d583a934a3fd1eb8db677
JASaddington <jasaddington@gmail.com>
2023-05-06 19:03:50 -0500

    starting js files, organizing structures and functions

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole} = require('./lib/choices')
 const mysql= require('mysql2');
 const inquirer = require('inquirer');
 
@@ -16,3 +16,50 @@ const db = mysql.createConnection(
   },
   console.log('//connected to personnel_db database!//')
 );
+
+function init() {
+  inquirer.createPromptModule([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments', 
+        'View all roles', 
+        'View all employees', 
+        'Add a department', 
+        'Add a role', 
+        'Add an employee', 
+        'Update an employee role', 
+      ]
+    }
+  ])
+  .then(function(answer) {
+    console.log(answer);
+    switch (answer.choice) {
+      case 'View all departments':
+        viewDepartments();
+        break;
+      case 'View all roles':
+        viewRoles();
+        break;
+      case 'View all employees':
+        viewEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateRole();
+        break;
+      default:
+        console.log('error, not a valid choice')
+
+    }
+  })
+}
